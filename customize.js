a1369e561ba83d94f46f9ed206939fa752863f20
jsiyo <mail.jsiyo@gmail.com>
2021-06-14 17:10:26 +0900

    update

--- customize.js
+++ customize.js
@@ -7,7 +7,7 @@ const config = {
   title: `gatsby-blog-mdx`,          // Displayed in header
   description: `This is my blog`,     // Site description
   maxWidth: "768px",                  // Max width of website
-  enableAbout: true,                  // Enables about page
+  enableAbout: false,                  // Enables about page
   siteUrl: `https://yourDomain.com/`, // For sitemap & RSS feed
   useScrollIndicator: true,           // Use scroll indicator on top of screen in posts
 
